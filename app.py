ee793d9334b413fe58737af1f7fe7e415b284b74
Kamaljeet <50160354+KamaljeetSahoo@users.noreply.github.com>
2021-08-09 02:40:53 +0530

    Code Added with streamlit integration

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from utils import process_text, generate_images, generate_pdf
+
+st.header('Text to Handwritting')
+
+input_text = st.text_area('Enter Your Text Here', height=200)
+
+if st.checkbox('Show Input Text'):
+    st.write(input_text)
+
+if st.checkbox('Convert'):
+    processed_text, words = process_text(input_text)
+    final_images = generate_images(words)
+
+    if st.checkbox('Show Images'):
+        for i in range(len(final_images)):
+            st.image(final_images[i], 'page_'+str(i+1), channels='BGR')
+
+    if st.button('Download PDF'):
+        link = generate_pdf(final_images)
+        st.markdown(link, unsafe_allow_html=True)
