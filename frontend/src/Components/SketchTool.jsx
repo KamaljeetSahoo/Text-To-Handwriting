790818c9fc13ab11db6c6af5ccd0ac87ebf69e9e
kLamda <krishnakumar76068@gmail.com>
2021-12-03 17:14:00 +0530

    controller part done

--- frontend/src/Components/SketchTool.jsx
+++ frontend/src/Components/SketchTool.jsx
@@ -1,8 +1,26 @@
 import * as React from "react";
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, Row, Col, Container} from 'react-bootstrap';
 import CanvasDraw from "react-canvas-draw";
+import Controller from "./controller";
 
 export default class Sketch extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          selectedIndex: 0,
+          options: Object.keys(props.data)
+      }
+  }
+
+  handleControl = (option) => {
+    let index = this.state.selectedIndex;
+    this.setState({selectedIndex: option===0 ? (index - 1 > 0 ? index - 1 : index) : (index + 1 < 26 ? index + 1 : index)});
+  }
+
+  handleSelect = (event) => {
+    this.setState({selectedIndex: event.target.value});
+  }
 
   render() {
     return (
@@ -12,13 +30,20 @@ export default class Sketch extends React.Component {
             <Modal.Title>Write A</Modal.Title>
           </Modal.Header>
           <Modal.Body className='center-align'>
-            <CanvasDraw
-              ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-              brushColor={'#ffc600'}
-              brushRadius={1}
-              canvasHeight={300}
-              saveData={this.props.data}
-            />
+            <Container>
+              <Controller selectedIndex={this.state.selectedIndex} handleControl={this.handleControl} options={this.state.options} handleSelect={this.handleSelect}/>
+              {/* <Row>
+                <Col>
+                  <CanvasDraw
+                    ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+                    brushColor={'#ffc600'}
+                    brushRadius={1}
+                    canvasHeight={300}
+                    saveData={this.props.data}
+                  />
+                </Col>
+              </Row> */}
+            </Container>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={this.props.handleSketchClose}>
